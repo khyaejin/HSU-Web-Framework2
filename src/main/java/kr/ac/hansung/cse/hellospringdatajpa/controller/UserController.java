7859ee3cdf6a642f5d592efa5cd5c00a64a447ad
khyaejin <olivia019182@naver.com>
2025-06-07 00:34:42 +0900

    ✨ Feat: 인증 관련 html 생성

--- src/main/java/kr/ac/hansung/cse/hellospringdatajpa/controller/UserController.java
+++ src/main/java/kr/ac/hansung/cse/hellospringdatajpa/controller/UserController.java
@@ -3,6 +3,7 @@ package kr.ac.hansung.cse.hellospringdatajpa.controller;
 import kr.ac.hansung.cse.hellospringdatajpa.dto.UserInfoDto;
 import kr.ac.hansung.cse.hellospringdatajpa.entity.UserRole;
 import kr.ac.hansung.cse.hellospringdatajpa.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,21 +11,29 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
-    // 회원가입
+    // 회원가입 폼 렌더링
+    @GetMapping("/register")
+    public String showRegisterForm(Model model) {
+        model.addAttribute("user", new UserInfoDto()); // ← 여기 반드시 있어야 함
+        return "register";
+    }
+
+    // ===================================
+
+    // 회원가입 기능 구현
     @PostMapping("/register")
     public String register(@ModelAttribute UserInfoDto userInfoDto, Model model) {
         userService.add(userInfoDto);
-        return "redirect:/login"; // 로그인 페이지로 이동
+        return "redirect:/users/login";
     }
 
-    // 로그인 (SecurityConfig에서 처리)
+    // 로그인 기능 구현
     @GetMapping("/login")
     public String loginPage() {
         return "login";
     }
-
 }
